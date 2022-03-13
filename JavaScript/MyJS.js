79b8e3db44e957cce0d14ead4adb72c7a473c71d
Alireza Ebrahimi <ali6648e@gmail.com>
2022-03-13 12:33:36 +0330

    new Project

---
+++ JavaScript/MyJS.js
@@ -0,0 +1,15 @@
+// function Sum() {
+//     var num1 = document.getElementById('number1').value;
+//     var num2 = document.getElementById('number2').value;
+//     var sum=parseInt(num1)+parseInt(num2);
+//     var lblresult = document.getElementById('result');
+//     lblresult.innerHTML =sum;
+// }
+
+var i = 12;
+var j = 24;
+
+debugger
+x = i + j;
+document.getElementById('result').innerHTML = x;
+
